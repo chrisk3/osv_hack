25c41ea99bb2de548a69cad7516ae5ffa683bdba
Aaron Young <aarongyoung85@gmail.com>
2013-11-17 14:00:37 -0800

    file upload done

--- edit.php
+++ edit.php
@@ -1,8 +1,9 @@
 <?php 
-	require('php/connection.php');
-	$connect = new Database();
-	$get_slide = "SELECT * FROM slides WHERE id = 1";
-	$slide = $connect->fetch_result($get_slide);
+session_start();
+require('php/connection.php');
+$connect = new Database();
+$get_slide = "SELECT * FROM slides WHERE id = 1";
+$slide = $connect->fetch_result($get_slide);
 ?>
 
 <html>
@@ -25,6 +26,25 @@
 				<li class='listitem' id='fifth' data-slideid=<?= $slide['id'] ?>><?= $slide['fifth'] ?></li>
 				<li class='listitem' id='sixth' data-slideid=<?= $slide['id'] ?>><?= $slide['sixth'] ?></li>
 			</ul>
+			<form id="pic" method="post" action="php/editslide.php" enctype="multipart/form-data">
+			<p>Uploade a picture: <input type="file" name="file"></p>
+			<p id="filestatus">
+				<?php 
+					if (isset($_SESSION['fileupload'])) {
+						echo $_SESSION['fileupload'];
+						unset($_SESSION['fileupload']);
+					}
+				?>
+			</p>
+			<input type='hidden' name='id' value=<?= $slide['id'] ?>>
+			<input type="submit" value="upload">
+		</form>
+		<div id="uploadpic"><img src=
+			<?php 
+				$query = "SELECT pic FROM slides WHERE id = {$slide['id']}"; 
+				$pic = $connect->fetch_result($query);
+				echo "../img/upload/".$pic['pic'];
+			?>></div>
 		</div>
 	</section>
 </body>
--- php/editslide.php
+++ php/editslide.php
@@ -1,4 +1,5 @@
 <?php 
+session_start();
 require('connection.php');
 $connect = new Database();
 
@@ -16,6 +17,36 @@ if (isset($_POST['id']) && isset($_POST['slideid'])) {
 	$get_new = "SELECT {$_POST['saveid']} FROM slides WHERE id = {$_POST['saveslideid']}";
 	$content = $connect->fetch_result($get_new);
 	echo $content["{$_POST['saveid']}"];
+} else if (isset($_FILES['file']) && isset($_POST['id'])) {
+	// File restrictions
+	$allowedExts = array("gif", "jpeg", "jpg", "png");
+	$temp = explode(".", $_FILES["file"]["name"]);
+	$extension = end($temp);
+	if ((($_FILES["file"]["type"] == "image/gif")
+	|| ($_FILES["file"]["type"] == "image/jpeg")
+	|| ($_FILES["file"]["type"] == "image/jpg")
+	|| ($_FILES["file"]["type"] == "image/pjpeg")
+	|| ($_FILES["file"]["type"] == "image/x-png")
+	|| ($_FILES["file"]["type"] == "image/png"))
+	// && ($_FILES["file"]["size"] < 20000)
+	&& in_array($extension, $allowedExts)) {
+		 if ($_FILES["file"]["error"] > 0) {
+	    	$_SESSION['fileupload'] = "Error: " . $_FILES["file"]["error"] . "<br>";
+		 } else {
+	   		if (file_exists("../img/upload/" . $_FILES["file"]["name"])) {
+	      		$_SESSION['fileupload'] = $_FILES["file"]["name"] . " already exists. ";
+	      		header("Location: ../edit.php");
+	      	} else {
+		      move_uploaded_file($_FILES["file"]["tmp_name"],
+		      "../img/upload/" . $_FILES["file"]["name"]);
+		      $_SESSION['fileupload'] = $_FILES["file"]["name"]." successfully uploaded";
+
+		      $query = "UPDATE slides SET pic = '{$_FILES['file']['name']}' WHERE id = '{$_POST['id']}'";
+		      $connect->run_query($query);
+		      header("Location: ../edit.php");
+		    }
+		 }
+	}
 }
 
 ?>
