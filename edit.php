e9476d2ef707f927308a771821c4467d2c60b262
Aaron Young <aarongyoung85@gmail.com>
2013-11-17 10:58:55 -0800

    database connection working

--- edit.php
+++ edit.php
@@ -1,6 +1,8 @@
 <?php 
 	require('php/connection.php');
 	$connect = new Database();
+	$get_slide = "SELECT * FROM slides WHERE id = 1";
+	$slide = $connect->fetch_result($get_slide);
 ?>
 
 <html>
@@ -11,7 +13,9 @@
 </head>
 <body>
 	<section>
-		
+		<div id='slide'>
+			<h1><?= $slide["title"] ?></h1>
+		</div>
 	</section>
 </body>
 </html>
